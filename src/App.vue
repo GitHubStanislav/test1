762d2815b48968635cdd32018a0992691a3f8d8e
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-03-01 17:47:17 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section v-cloak>
+    <h1>{{ name }}</h1>
+    <input v-model="inputValue"/>
+    <button @click="clearField">Clear this</button>
+    <p v-if="inputValue">Some text: {{ inputValue }}</p>
+    <p v-else>Some text will be here:</p>
+  </section>
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Hello world',
+      inputValue: ''
+    }
+  },
+  methods: {
+    clearField() {
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<style>
+[v-cloack] {
+  display: none;
+}
+</style>
